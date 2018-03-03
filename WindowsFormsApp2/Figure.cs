ad4cc03ceabc3d1e1856e9d63e4221a0623a80d0
UDK <udk1@bk.ru>
2018-03-03 16:18:38 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Figure.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class figura 
+    {
+        private Point point;
+        public Point POint
+        {
+            get
+            {
+                return point;
+            }
+        }
+        public Pen pen;
+        public void Point(int x, int y)
+        {
+            point.X = x;
+            point.Y = y;
+        }
+    }
+    class quad:figura
+    {
+        public Rectangle rectangle;
+        public Size size;
+    }
+    class НарисоватьДоску
+    {
+        public quad СделатьДоску(int X, int Y)
+        {
+
+
+
+            quad circle = new quad();
+            //Цвет
+            Pen pen = new Pen(Color.Black);
+            //Ширина пера
+            pen.Width = 2.16f;
+            circle.pen = pen;
+            //Размеры круга
+            circle.rectangle.Width = 10;
+            circle.size.Width = 30;
+            circle.size.Height = 30;
+            circle.rectangle.Size = circle.size;
+            //где находится круг
+            circle.Point(X, Y);
+            return circle;
+        }
+    }
+}
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        Graphics graf;
+        Pen peen;
+        int a, b;
+        SolidBrush white, black;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Paint += PictureBox1_Paint;
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graf = e.Graphics;
+            НарисоватьДоску qq = new НарисоватьДоску();
+            quad kryg = qq.СделатьДоску(100,100);
+
+            graf.DrawRectangle(kryg.pen, kryg.rectangle);
+            //throw new NotImplementedException();
+        }
+    }
+}
